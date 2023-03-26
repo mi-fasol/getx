faf03d7cc550792944bbb2345fccbdcc411435f7
mi-fasol <minyoong39@naver.com>
2023-03-26 23:32:05 +0000

    feat: 반응형 상태 관리 추가

---
+++ lib/state/reactive_getx.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/controller/reactive_controller.dart';
+
+class ReactiveGetX extends StatelessWidget {
+  const ReactiveGetX({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(ReactiveController());
+
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GetBuilder<ReactiveController>(builder: (controller) {
+          return Text(
+            "${controller.count}",
+            style: const TextStyle(
+              fontSize: 50,
+            ),
+          );
+        }),
+        ElevatedButton(
+            onPressed: controller.increase,
+            child: const Text(
+              "+",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ))
+      ],
+    ));
+  }
+}
